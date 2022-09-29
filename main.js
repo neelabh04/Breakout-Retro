f7f8b917354208f6c477e5af8ba7565bf2699007
Neelabh Singh <neelabh041997@gmail.com>
2022-09-29 09:31:10 +0530

    Defined referenced for a block on grid

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
 const grid = document.querySelector('.grid');
 const block = document.createElement('div');
+const blockWidth = 100;
+const blockHeight = 20;
 
-block.classList.add('block');
-block.style.left = '100px';
-block.style.bottom = '50px';
+// create block
+class Block {
+    constructor(xAxis, yAxis) {
+        this.bottomLeft = [xAxis, yAxis];
+        this.bottomRight = [xAxis + blockWidth, yAxis];
 
-grid.appendChild(block);
+        this.topLeft = [xAxis, yAxis + blockHeight];
+        this.topRight = [xAxis + blockWidth, yAxis + blockHeight];
+    }
+}
+
+// all blocks 
+const blocks = [
+    new Block(10, 270)
+]; 
+
+console.log(blocks[0]);
+
+// draw all blocks
+// function addBlocks() {
+//     for(let i = 0; i < blocks.length; i++) {
+//         block.classList.add('block');
+//         block.style.left = blocks[i].bottomLeft[0];
+//         block.style.bottom = ;
+//         grid.appendChild(block);
+
+//     }
+// }
+
+// addBlocks();
