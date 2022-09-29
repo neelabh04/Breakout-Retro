0862bae3a60f7b7e26f781eb4115a0f75ad3d448
Neelabh Singh <neelabh041997@gmail.com>
2022-09-29 09:42:14 +0530

    Grid layout and CSS tweaked

--- main.js
+++ main.js
@@ -16,20 +16,38 @@ class Block {
 
 // all blocks 
 const blocks = [
-    new Block(10, 270)
+    new Block(10, 270),
+    new Block(120, 270),
+    new Block(230, 270),
+    new Block(340, 270),
+    new Block(450, 270),
+    
+    new Block(10, 240),
+    new Block(120, 240),
+    new Block(230, 240),
+    new Block(340, 240),
+    new Block(450, 240),
+    
+    new Block(10, 210),
+    new Block(120, 210),
+    new Block(230, 210),
+    new Block(340, 210),
+    new Block(450, 210),
 ]; 
 
-console.log(blocks[0]);
 
 // draw all blocks
-// function addBlocks() {
-//     for(let i = 0; i < blocks.length; i++) {
-//         block.classList.add('block');
-//         block.style.left = blocks[i].bottomLeft[0];
-//         block.style.bottom = ;
-//         grid.appendChild(block);
+function addBlocks() {
+    for(let i = 0; i < blocks.length; i++) {
+        const block = document.createElement('div');
 
-//     }
-// }
+        block.classList.add('block');
+        block.style.left = blocks[i].bottomLeft[0] + 'px';
+        block.style.bottom = blocks[i].bottomLeft[1] + 'px';
 
-// addBlocks();
+        grid.appendChild(block);
+
+    }
+}
+
+addBlocks();
